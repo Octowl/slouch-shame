7b2675bbc994018600ace644b5b8519c2da19e37
Octowl <alsaff1987@gmail.com>
2016-05-06 15:25:37 -0400

    Overwrite with accelerometer module

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-// Import the interface to Tessel hardware
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+/*********************************************
+This basic accelerometer example logs a stream
+of x, y, and z data from the accelerometer
+*********************************************/
+
 var tessel = require('tessel');
+var accel = require('accel-mma84').use(tessel.port['A']);
 
-// Turn one of the LEDs on to start.
-tessel.led[2].on();
+// Initialize the accelerometer.
+accel.on('ready', function () {
+    // Stream accelerometer data
+  accel.on('data', function (xyz) {
+    console.log('x:', xyz[0].toFixed(2),
+      'y:', xyz[1].toFixed(2),
+      'z:', xyz[2].toFixed(2));
+  });
 
-// Blink!
-setInterval(function () {
-  tessel.led[2].toggle();
-  tessel.led[3].toggle();
-}, 100);
+});
 
-console.log("I'm blinking! (Press CTRL + C to stop)");
+accel.on('error', function(err){
+  console.log('Error:', err);
+});
