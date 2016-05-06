54ce082cb425c09d7ca8a623adafbc9d5913bb8b
Octowl <alsaff1987@gmail.com>
2016-05-06 16:14:56 -0400

    Setup slouch meter

--- index.js
+++ index.js
@@ -9,17 +9,23 @@ of x, y, and z data from the accelerometer
 var tessel = require('tessel');
 var accel = require('accel-mma84').use(tessel.port['A']);
 
+var origin = 1;
+var slouchMeter = 0;
+var deltaX, currX;
+
 // Initialize the accelerometer.
 accel.on('ready', function () {
     // Stream accelerometer data
-  accel.on('data', function (xyz) {
-    console.log('x:', xyz[0].toFixed(2),
-      'y:', xyz[1].toFixed(2),
-      'z:', xyz[2].toFixed(2));
-  });
+    console.log('Ready to record!');
+    accel.on('data', function (xyz) {
+        currX = +(xyz[0].toFixed(2))
+        deltaX = Math.abs(currX) - origin;
+        // slouchMeter += Math.abs(deltaX) > 1 ? deltaX : 0;
+        if (deltaX > .05) { console.log(deltaX) }
+    });
 
 });
 
-accel.on('error', function(err){
-  console.log('Error:', err);
+accel.on('error', function (err) {
+    console.log('Error:', err);
 });
